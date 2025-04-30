5fcb12a7946dbef4ec0f6288cbe2d04a86aaba0e
murilo-possidonio <muriloricardotininho@gmail.com>
2025-04-30 17:54:58 +0000

    aula30.04.25

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
+app.get('/', (request, response) => {
+    //response.send('Testando!!!');
+    response.render('home');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor na porta 3000');
+});
