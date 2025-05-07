88661f08f9b8ae8ff916b18b3d0d5095c13188b6
murilo-possidonio <muriloricardotininho@gmail.com>
2025-05-07 17:49:55 +0000

    aula 07/05/25

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ app.set('views', './views')
 
 app.get('/', (request, response) => {
     //response.send('Testando!!!');
-    response.render('home');
+    response.render('home', {nome : "Murilo"});
+});
+
+app.get('/tabuada', (request, response) => {
+      let resultado = [];
+      for(let cont = 1;cont<=10;cont++){
+          resultado.push(cont * 5);
+      }
+      response.render('tabuada', {valores : resultado});
 });
 
 app.listen(3000, () => {
