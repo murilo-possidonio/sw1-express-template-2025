75e9f34c435241c821627af07399b0c5e807af40
murilo-possidonio <muriloricardotininho@gmail.com>
2025-05-14 17:49:50 +0000

    aula14.05

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
-
 const app = express()
-
 app.set('view engine', 'hbs')
 app.set('views', './views')
+
+app.use(express.static('piblic'));
 
 app.get('/', (request, response) => {
     //response.send('Testando!!!');
     response.render('home', {nome : "Murilo"});
 });
 
-app.get('/tabuada', (request, response) => {
+
+
+app.get('/tabuada/:numero', (request, response) => {
       let resultado = [];
-      for(let cont = 1;cont<=10;cont++){
-          resultado.push(cont * 5);
+      const numero = request.params['numero']
+      for(let cont = 1;cont<=10; cont++){
+          resultado.push(cont * numero);
       }
       response.render('tabuada', {valores : resultado});
 });
 
 app.listen(3000, () => {
-    console.log('Servidor na porta 3000');
+    console.log('Servidor na porta3000');
 });
